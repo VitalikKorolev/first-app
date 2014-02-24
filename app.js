7cca3bb9f98d924c8283c9fd3652900318df6a4a
VitalikKorolev <nodesite1@gmail.com>
2014-02-24 18:14:57 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express')
+   , stylus = require('stylus');
+
+var app = module.exports = exports = express();
+
+// Configuration
+
+app.set('port', process.env.PORT || 8456);
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// Middleware
+
+app.use(express.logger('dev'));
+
+app.use(app.router);
+
+app.use(stylus.middleware({
+    src: './public',
+    compress: true
+}));
+
+app.use(express.static('./public'));
+
---
+++ routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var app = require('./app');
+
+app.get('/', function(req, res, next) {
+    req.url = '/index.html';
+    next();
+});
+
+app.get('/*.html', function(req, res, next) {
+    res.render('pages/' + req.params[0]);
+});
